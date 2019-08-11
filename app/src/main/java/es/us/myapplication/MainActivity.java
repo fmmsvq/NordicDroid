4fc5ab8319414631298d6234a4659afaad58b25c
fmm.svq <fmm.svq@gmail.com>
2019-08-11 06:34:17 -0500

    Actualizaciones. Boton que lleva de Ventana 1 a ventana 2 (Arreglada colocacion)

--- app/src/main/java/es/us/myapplication/MainActivity.java
+++ app/src/main/java/es/us/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package es.us.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveToActivity2();
+            }
+        });
+    }
+
+    private void moveToActivity2(){
+        Intent intent = new Intent(MainActivity.this, Activity2DispositivosBT.class);
+        startActivity(intent);
     }
 }
